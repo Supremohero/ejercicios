afc2bf6c0ce52b5994d4a175204e4d348323d9e4
Dani <danitherequiem@gmail.com>
2017-02-24 13:27:57 +0100

    Actualizado

---
+++ tema6/profile.php
@@ -0,0 +1,10 @@
+</form>
+		</div>
+
+		<h2 class="titlat">Perfil</h2>
+		<div id="registro" class="cuerpolateral">
+			<h1>Bienvenido <?php echo $_SESSION['login_user'] ?> </h1>
+			<a href="logout.php"><h3>Cerrar sesión</h3></a>
+
+		</div>
+
