cce53226819cc5179a135c871c0d8fa5150b82b8
Dani <danitherequiem@gmail.com>
2017-02-25 21:48:30 +0100

    connection duplicado arreglado

--- tema6/cabecera.php
+++ tema6/cabecera.php
@@ -42,8 +42,6 @@ if (isset($_POST['submit'])) {
 		$username=$_POST['username'];
 		$password=$_POST['password'];
 		$username = strtolower($username);
-		// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-		$connection = @mysql_connect("localhost", "root", "");
 		// To protect MySQL injection for Security purpose
 		$username = stripslashes($username);
 		$password = stripslashes($password);
