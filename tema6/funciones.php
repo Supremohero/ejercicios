368c4cda00eaf8b7fa68a1e00a4d3aa2b0faa502
Dani <danitherequiem@gmail.com>
2017-05-02 19:43:39 +0200

    Modificada la base de datos MySQL a la de Hostinger.

--- tema6/funciones.php
+++ tema6/funciones.php
@@ -1,9 +1,12 @@
 <?php
 session_start(); // Starting Session
-define ("HOSTNAME","localhost");
-define ("DATABASE","curso_php");
-define ("USER_DB","root");
-define ("PASSWORD_DB","");
+define ("HOSTNAME","mysql.hostinger.es");
+define ("DATABASE","u963447591_dani");
+define ("USER_DB","u963447591_root");
+define ("PASSWORD_DB","tooroot");
+
+
+
 function parametro_plantilla($variable){
 	if (isset($GLOBALS[$variable])) {
 		echo $GLOBALS[$variable];
